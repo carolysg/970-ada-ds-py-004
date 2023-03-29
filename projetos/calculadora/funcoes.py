ffd9a659e4956555f70ca93b13175a2f117a7662
Carolina Yumi S. Goshima <carolinagoshima@gmail.com>
2023-03-29 15:02:16 -0300

    Definindo função subtração

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -5,3 +5,6 @@ def soma(a, b):
         return a + b
     except TypeError:
         print(f"O input 'a' e 'b' devem ser int ou float, recebido {a} tipo {type(a)}, {b} tipo {type(b)}.")
+
+def subtracao(a, b):
+    return a - b
