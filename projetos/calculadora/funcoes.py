a695e5742caa6edf37088bded7de3191247341f0
Carolina Yumi S. Goshima <carolinagoshima@gmail.com>
2023-03-29 15:06:54 -0300

    Definindo função divisão

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -8,3 +8,10 @@ def soma(a, b):
 
 def subtracao(a, b):
     return a - b
+
+def divisao(a, b):
+    if b != 0:
+        return a / b
+    else:
+        print('Erro: Divisão inválida!')
+        return 0 
