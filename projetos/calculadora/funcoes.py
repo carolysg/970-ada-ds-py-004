5d69a05c9bf955fd71d2c0263c9be3e9853811bf
Carolina Yumi S. Goshima <carolinagoshima@gmail.com>
2023-03-29 15:19:55 -0300

    Corrigindo as funções

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -4,7 +4,7 @@ def soma(a, b):
             raise TypeError
         return a + b
     except TypeError:
-        print(f"O input 'a' e 'b' devem ser int ou float, recebido {a} tipo {type(a)}, {b} tipo {type(b)}.")
+        return f"O input 'a' e 'b' devem ser int ou float, recebido {a} tipo {type(a)}, {b} tipo {type(b)}."
 
 def subtracao(a, b):
     return a - b
@@ -22,4 +22,4 @@ def multiplicacao(a, b):
             raise TypeError
         return a * b
     except TypeError:
-        print(f"O input 'a' e 'b' devem ser int ou float, recebido {a} tipo {type(a)}, {b} tipo {type(b)}.")
+        return f"O input 'a' e 'b' devem ser int ou float, recebido {a} tipo {type(a)}, {b} tipo {type(b)}."
